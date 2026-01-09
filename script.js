0bf731ba9e20e460aca6ba37147cf20b9e1523bf
Rahul Jangid <163715859+Rahul04jangid@users.noreply.github.com>
2026-01-09 15:59:53 +0530

    add uploads

---
+++ script.js
@@ -0,0 +1,14 @@
+/*==============================typing animation=======================*/
+// var typed = new Typed(".typing",{
+//     strings:["","Gamer","Web Developer","Graphic Designer","Youtuber"],
+//     typeSpeed:100,
+//     backSpeed:60,
+//     loop:true
+// }) ;
+
+var typed = new Typed(".typing", {
+    strings: ["Gamer", "Web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
